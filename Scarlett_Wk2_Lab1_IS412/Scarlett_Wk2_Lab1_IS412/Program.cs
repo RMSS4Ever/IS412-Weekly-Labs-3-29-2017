b2b1286514d36250ecedfd9e42b46391a5043cf5
RMSS4Ever <bscarlett2008@yahoo.com>
2017-04-10 10:27:25 -0700

    Week 2 Lab 1 - Print out reversed name

---
+++ Scarlett_Wk2_Lab1_IS412/Scarlett_Wk2_Lab1_IS412/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Lab Assignment for IS412 Week 2
+ * Lab 1
+ * 
+ * Brian Scarlett * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scarlett_Wk2_Lab1_IS412
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string YourName;
+            string reversedString = "";
+
+            Console.WriteLine("What is your name? ");
+            YourName = Console.ReadLine();
+
+            // Create array to print name backwards
+            for(int i = YourName.Length -1; i >= 0; i--)
+            {
+                reversedString += YourName[i];
+            }
+
+            // Print name out backwards
+            Console.WriteLine($"Hello { YourName}. Your name spelled backwards looks like: {reversedString}");
+
+            // System pause
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Scarlett_Wk2_Lab1_IS412/Scarlett_Wk2_Lab1_IS412/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Scarlett_Wk2_Lab1_IS412")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Scarlett_Wk2_Lab1_IS412")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a32699b-5b61-42c2-9e95-69b5be35f80b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

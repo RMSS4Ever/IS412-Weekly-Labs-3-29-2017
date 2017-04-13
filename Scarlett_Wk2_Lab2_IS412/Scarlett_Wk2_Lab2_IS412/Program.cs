c1b32550979b341ec5e6e5720a64abd8f0056bb2
RMSS4Ever <bscarlett2008@yahoo.com>
2017-04-12 17:58:58 -0700

    TRY IT OUT: Objects in Action: Ch08Ex01

---
+++ Scarlett_Wk2_Lab2_IS412/Scarlett_Wk2_Lab2_IS412/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Lab Assignment for IS412 Week 2
+ * Lab 2 
+ * - Accept a string and replaces all instances of the string false with true.
+ * 
+ * Brian Scarlett * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scarlett_Wk2_Lab2_IS412
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string myString;
+
+            // User enter sentence containing the word "false"
+            Console.WriteLine("Enter a sentence: ");
+            myString = Console.ReadLine();
+
+            // Convert all to lower case
+            myString = myString.ToLower();
+
+            // Replace all instances of "false" with "true"
+            myString = myString.Replace("false", "true");
+                        
+            // Print result
+            Console.WriteLine($"Replaced \"false\" with \"true\": {myString}");
+            
+            // System pause
+            Console.ReadKey();
+
+        }
+    }
+}
